cc43df8833d534e1fad30e48783b3aaa5f6d63a0
Mike Novitsky (mnovitsk) <mnovitsk@cisco.com>
2020-01-21 17:45:04 -0500

    initial commit

---
+++ macLookup.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"log"
+    "net/url"
+	"net/http"
+	// "encoding/json"
+)
+
+func getInputMac() string {
+	
+	inputMac := "44:38:39:ff:ef:57"
+	
+	if len(os.Args) > 1 {
+		inputMac = os.Args[1]
+		fmt.Printf("Mac Address supplied: %s\n", inputMac)
+	} else {
+    	fmt.Printf("No Mac Address supplied, using default: %s\n", inputMac)
+  	}
+
+  	return inputMac
+}
+
+func buildUrl() string {
+
+	macApiKey, found := os.LookupEnv("macApiKey")
+
+	if !found {
+		log.Fatalf("Missing macApiKey env variable")
+	}
+
+	macAddress := getInputMac()
+
+	//fmt.Printf("Mac Address Lookup API Key: %s\n", macApiKey)
+
+	baseUrl, err := url.Parse("https://api.macaddress.io/v1")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	params := url.Values{}
+	params.Add("search", macAddress)
+	params.Add("apikey", macApiKey)
+	params.Add("output", "vendor")
+
+	fmt.Printf("Querying: %s\n", baseUrl)
+
+	baseUrl.RawQuery = params.Encode()
+	
+	return baseUrl.String()
+}
+
+func getMacInfo(queryUrl string) string {
+	resp, err := http.Get(queryUrl)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body)
+}
+
+func main() {
+	fmt.Printf("\nBeginning Execution\n\n")
+
+	testUrl := buildUrl()
+
+	//fmt.Printf("Query URL built: %s\n", testUrl)
+
+	macReturn := getMacInfo(testUrl)
+
+	fmt.Printf("Vendor: %s\n", macReturn)
+}
